e8135b3eec28605b681a3cf4d30f7c2366b1e0d1
Kirill <ktoto1093@gmail.com>
2026-09-20 12:13:31 +0300

    пройдена проверка pytest-cov, которая показала 100%; файлы '__int__' переименованы в '__init__.py'

--- tests/test_code.py
+++ tests/test_code.py
@@ -1,8 +1,8 @@
 import pytest
 
-from masks import get_mask_card_number, get_mask_account
-from widget import mask_account_card, get_date
-from processing import filter_by_state, sort_by_date
+from src.masks import get_mask_card_number, get_mask_account
+from src.widget import mask_account_card, get_date
+from src.processing import filter_by_state, sort_by_date
 
 
 
